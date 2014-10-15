76997c8b413f5821c86e2ce9b5efac0721d4cdd9
Greg Laabs <OverloadUT@gmail.com>
2014-10-15 16:37:27 -0700

    Adding test stubs

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OATBeanCounter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BCDataStorage data = OATBeanCounterData.data;
+            data.OnEncodeToConfigNode();
+            Assert.AreEqual(data.data_version, "0.01");
+        }
+    }
+}
