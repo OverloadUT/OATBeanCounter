210a390c18a3e15814956be30326f970e3fe74da
Greg Laabs <OverloadUT@gmail.com>
2014-10-25 09:14:49 -0700

    Stub for BCGUI class
    Will contain the logic for drawing tables and stuff

---
+++ BCGUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OATBeanCounter
+{
+    public class BCGUI
+    {
+        //TODO Do I need this?
+    }
+
+    public class BCGUITable
+    {
+
+        public void Draw()
+        {
+
+        }
+    }
+}
